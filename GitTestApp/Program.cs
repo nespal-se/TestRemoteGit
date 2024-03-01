0e6cbb4a200ace3cb9de32ad9c07ce9a51bcf7e1
Nikita B <78895041+nespal-se@users.noreply.github.com>
2024-03-01 17:35:35 +0500

    Add files via upload

---
+++ GitTestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Я готов!");
+            // вторая строка в консоли
+            Console.WriteLine("Еее!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GitTestApp/Questioner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestApp
+{
+    internal class Questioner
+    {
+        public static void AskQuestion()
+        {
+            Console.WriteLine("Как дела?");
+        }
+    }
+}
